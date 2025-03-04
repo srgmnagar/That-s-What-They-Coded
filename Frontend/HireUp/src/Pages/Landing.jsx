9ac78cc8a835a7a7bda7811f1a6834178b60345a
srgmnagar <nagarsrgm@gmail.com>
2025-03-04 23:37:51 +0530

    Add blue button gradient image and update Landing page layout

--- Frontend/HireUp/src/Pages/Landing.jsx
+++ Frontend/HireUp/src/Pages/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import bg from '../Images/Landing/bg.png'
 import logo from '../Images/Landing/logo.png'
+import { NavLink } from 'react-router-dom'
+import blue_button_gradient from '../Images/Landing/blue_button_gradient.png'
 
 const Landing = () => {
     return (
@@ -14,26 +16,38 @@ const Landing = () => {
                 }}
                 className='w-full h-[120vh] opacity-100'
             >
-                <div className='flex justify-end items-center px-12 py-3'>
-                    <img src={logo} className='w-44' alt="" />
+                <div className='flex justify-end items-center px-5 sm:px-12 py-3'>
+                    <img src={logo} className='sm:w-44 w-28' alt="" />
                 </div>
-                <section className='w-11/12 lg:w-2/5 lg:ml-44'>
-                <div className='flex flex-col items-left gap-6 '>
-                    <h2 className='text-6xl font-medium font-Orbitron'>
-                        <span className='bg-gradient-to-r from-[#00C2FF] to-[#E100FF] text-transparent bg-clip-text'>
-                        SKILLS OVER RESUMES
-                        </span>
-                    </h2>
-                    <h2 className='text-3xl font-light font-Sora'>
-                        <span className='bg-gradient-to-r from-[#00C2FF] to-[#E100FF] text-transparent bg-clip-text'>
-                        Earn XP, Get Hired & Build Your Future
-                        </span>
-                    </h2>
+                <section className='max-w-[90%] md:max-w-[60%] sm:mx-12 mx-10 xl:ml-44'>
+                    <div className='flex flex-col items-left justify-start gap-6 '>
+                        <h2 className='xl:text-6xl md:text-5xl text-4xl font-medium font-Orbitron flex flex-col'>
+                            <span className='bg-gradient-to-r from-[#00C2FF] to-[#E100FF] text-transparent bg-clip-text'>
+                                SKILLS OVER
+                            </span>
+                            <span className='bg-gradient-to-r from-[#00C2FF] to-[#E100FF] text-transparent bg-clip-text'>
+                                RESUMES
+                            </span>
+                        </h2>
+                        <h2 className='xl:text-3xl sm:text-2xl text-xl font-light font-Sora'>
+                            <span className='bg-gradient-to-r from-[#00C2FF] to-[#E100FF] text-transparent bg-clip-text'>
+                                Earn XP, Get Hired & Build Your Future
+                            </span>
+                        </h2>
 
-                </div>
+                    </div>
                 </section>
+                <NavLink
+                    className="text-white block text-center flex items-center justify-center"
+                    style={{
+                        backgroundImage: `url(${blue_button_gradient})`,
+                    }}
+                >
+                    Take a Test
+                </NavLink>
 
             </header>
+
         </div>
     )
 }
